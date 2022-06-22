b307eb8513b13b2f2b57626a25b2338c1c71d5fd
Abhishek kumar <abhisheknick116@gmail.com>
2022-06-22 14:28:32 +0530

    'DPProblem'

---
+++ src/leetcodeDP/coinChange.java
@@ -0,0 +1,22 @@
+package leetcodeDP;
+import java.util.*;
+
+public class coinChange {
+    public int coinChange(int[] coins, int amount) {
+        if(amount == 0)
+            return 0;
+        if(coins.length == 0){
+            return 0;
+        }
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for(int i = 0; i < coins.length; i++){
+            for(int j = coins[i]; j < dp.length; j++){
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        return dp[amount] == amount + 1? -1: dp[amount];
+    }
+}
