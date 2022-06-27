8561322c86e6fdec1d925eb6ffb957c06f315b14
Abhishek kumar <abhisheknick116@gmail.com>
2022-06-27 18:55:03 +0530

    'Array'

---
+++ src/ArrayLeetcode/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,13 @@
+package ArrayLeetcode;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+    public int minPartitions(String n) {
+        int max = 0;
+        for(int i = 0; i < n.length(); i++){
+            int ch = n.charAt(i) - '0';
+
+            max = Math.max(max, ch);
+        }
+        return max;
+    }
+}
