08a7061db4291282513b0b98a77c9b331fe9724c
Abhishek kumar <abhisheknick116@gmail.com>
2022-06-29 22:29:26 +0530

    'Array'

---
+++ src/ArrayLeetcode/QueueReconstructionbyHeight.java
@@ -0,0 +1,14 @@
+package ArrayLeetcode;
+import java.util.*;
+public class QueueReconstructionbyHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        List<int[]> list = new LinkedList<>();
+        for(int[] person : people)
+        {
+            list.add(person[1], person);
+        }
+        return list.toArray(new int[list.size()][]);
+
+    }
+}
